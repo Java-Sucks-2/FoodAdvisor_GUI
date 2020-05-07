e939e8e78593492bfb97766fdfc2620825f5e0a2
Kawa-git <56511237+Kawa-git@users.noreply.github.com>
2020-05-07 16:07:16 +0200

    test

---
+++ src/components/FButton.java
@@ -0,0 +1,14 @@
+package src.components;
+
+import javax.swing.JButton;
+
+public class FButton extends JButton {
+
+	/***/
+	private static final long serialVersionUID = 1L;
+
+    public FButton(String text) {
+
+    }
+
+}
