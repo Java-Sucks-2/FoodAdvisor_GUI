5035e32ef0353f6c42206099f39f3920a3707b4d
Borotalcohol <loschiavo.christian@gmail.com>
2020-06-16 17:46:03 +0200

    Aggiunto componente lista

---
+++ src/gui/components/FList.java
@@ -0,0 +1,32 @@
+package src.gui.components;
+
+import java.awt.Font;
+import java.awt.Color;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.SwingConstants;
+
+public class FList extends JList<String> {
+    /***/
+    private static final long serialVersionUID = 1L;
+
+    public FList() {
+        super();
+        addStyle();
+    }
+
+    public FList(String[] items) {
+        super(items);
+        addStyle();
+    }
+
+    public void addStyle() {
+        this.setFont(new Font("Manrope", Font.PLAIN, 22));
+        this.setOpaque(false);
+        this.setBackground(Color.WHITE);
+        DefaultListCellRenderer renderer = (DefaultListCellRenderer) this.getCellRenderer();
+        renderer.setHorizontalAlignment(SwingConstants.CENTER);
+    }
+}
