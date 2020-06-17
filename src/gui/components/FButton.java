893d875ebbfa939a489ce3ec783cfe09d60f34e2
Borotalcohol <loschiavo.christian@gmail.com>
2020-06-17 17:37:05 +0200

    Cambio colore sul focus

--- src/gui/components/FButton.java
+++ src/gui/components/FButton.java
@@ -5,6 +5,9 @@ import java.awt.Dimension;
 import java.awt.Font;
 import javax.swing.JButton;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class FButton extends JButton {
     /***/
 	private static final long serialVersionUID = 1L;
@@ -22,6 +25,21 @@ public class FButton extends JButton {
         this.setContentAreaFilled(true);
         this.setBorderPainted(false);
         this.setPreferredSize(new Dimension(100, 45));
+
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(final MouseEvent e) {
+                setBackground(new Color(196, 48, 31));
+            }
+            public void mouseExited(final MouseEvent e) {
+                setBackground(new Color(237, 71, 53));
+            }
+        });
+
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseReleased(final MouseEvent arg0) {
+                setBackground(new Color(237, 71, 53));
+            }
+          });
     }
 
     public void autoSetBounds(int win_width, int width, int height, int ycoord) {
