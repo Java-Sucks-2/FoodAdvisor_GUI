62fdb9f6136ca8c0dac81e98d126cada2bab69ba
andrea1707 <andrea.ferrario170700@gmail.com>
2020-06-17 11:22:50 +0200

    -

--- src/clienti/Clienti.java
+++ src/clienti/Clienti.java
@@ -13,7 +13,6 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
--- src/gui/components/FList.java
+++ src/gui/components/FList.java
@@ -4,7 +4,6 @@ import java.awt.Font;
 import java.awt.Color;
 
 import javax.swing.DefaultListCellRenderer;
-import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.SwingConstants;
 
